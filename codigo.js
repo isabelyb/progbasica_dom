f3c9cecab00d5feee78063ec6111f147f36b33d7
Isabel Yepes <63390283+isabelyb@users.noreply.github.com>
2020-09-11 17:00:56 -0700

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,6 @@
+var ruta = window.location;
+console.log(ruta);
+document.write("Estas en: " + ruta);
+var ruta_n = navigator.NavigatorID;
+console.log(ruta_n);
+document.write("   navigator en: " + ruta_n);
